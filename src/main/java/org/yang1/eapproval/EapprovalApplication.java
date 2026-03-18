82449f5668a993582499197f0d7235c2fa9fbd97
Yang1-_- <wonjjang44@gmail.com>
2026-03-19 01:00:30 +0900

    chore: 프로젝트 세팅 파일 추가 - 의존성 추가

---
+++ src/main/java/org/yang1/eapproval/EapprovalApplication.java
@@ -0,0 +1,13 @@
+package org.yang1.eapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapprovalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yang1/eapproval/EapprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yang1.eapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
