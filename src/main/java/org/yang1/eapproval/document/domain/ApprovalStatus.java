dff140510b687886f318c8de01eb330230ae53b9
Yang1-_- <wonjjang44@gmail.com>
2026-05-13 14:46:27 +0900

    refactor: Entity 재설계로 인한 기존 엔티티 일괄 제거
    - 첨부파일(attachments) 엔티티 1차 기능 개발에서 제외

--- src/main/java/org/yang1/eapproval/document/domain/ApprovalStatus.java
+++
@@ -1,9 +0,0 @@
-package org.yang1.eapproval.document.domain;
-
-public enum ApprovalStatus {
-
-    IN_PROGRESS, // 결재 진행 중
-    APPROVED, // 승인
-    REJECTED, // 반려
-    ;
-}
--- src/main/java/org/yang1/eapproval/user/domain/UserRole.java
+++
@@ -1,8 +0,0 @@
-package org.yang1.eapproval.user.domain;
-
-public enum UserRole {
-
-    USER, // 일반사용자
-    ADMIN // 관리자
-    ;
-}
