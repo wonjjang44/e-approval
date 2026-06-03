e2fa90e1cdae0c1cba829c4b73a0407c2f59dcab
Yang1-_- <wonjjang44@gmail.com>
2026-06-03 16:00:34 +0900

    feat: User Entity 생성
    - User 역할 enum 생성

---
+++ src/main/java/org/yang1/eapproval/user/domain/entity/User.java
@@ -0,0 +1,74 @@
+package org.yang1.eapproval.user.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.yang1.eapproval.common.entity.BaseEntity;
+import org.yang1.eapproval.department.domain.entity.Department;
+import org.yang1.eapproval.user.domain.status.UserRole;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String loginId;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String userName;
+
+    @Column(length = 100)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private UserRole role;
+
+    @Column(length = 50)
+    private String positionName;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private User(String loginId, String password, String userName, String email, Department department, UserRole role, String positionName) {
+        this.loginId = loginId;
+        this.password = password;
+        this.userName = userName;
+        this.email = email;
+        this.department = department;
+        this.role = role;
+        this.positionName = positionName;
+        this.isActive = true;
+    }
+
+
+    public static User create(String loginId, String password, String userName, String email, Department department, UserRole role, String positionName) {
+        return User.builder()
+                .loginId(loginId)
+                .password(password)
+                .userName(userName)
+                .email(email)
+                .department(department)
+                .role(role)
+                .positionName(positionName)
+                .build();
+    }
+
+}
---
+++ src/main/java/org/yang1/eapproval/user/domain/status/UserRole.java
@@ -0,0 +1,7 @@
+package org.yang1.eapproval.user.domain.status;
+
+public enum UserRole {
+
+    USER,
+    ADMIN
+}
